96281e4b3711137ae17bf8e4334747d07c0ccd06
runningapple <runningapple8@gmail.com>
2016-02-24 13:44:24 +0800

    登录操作从后台验证信息 2016-2-24 13:45

--- js/login.js
+++ js/login.js
@@ -1,19 +1,24 @@
 
 $(document).ready(function(){
 	$("#loginbtn").tap(function(){
+		var account = $("#username").val();
+		var password = $("#pwd").val();
 		$.ajax({
 			type:"GET",
 			
 			/*这里需要进行跨域请求，论文里可以加以展开说明*/
-			url:"http://localhost:8080/nitshare/serve/test",
+//			url:"http://localhost:8080/nitshare/serve/test",
+			url:"http://localhost:8080/nitshare/serve/user.login",
 			//async:false,/*同步*/
 			jsonpCallback:'callback',
 			data:{
-				"id":"abc"
+				"account" : account,
+				"password" : password
 			},
 			dataType:'jsonp',
 			success : function(data){
-				alert("ok");
+				
+				alert(data[0].id);
 			},
 			error : function(XMLHttpRequest, textStatus, errorThrown, data){
 //				alert("本宝宝不开心了");
