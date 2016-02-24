3a94aefc1d3b68511c58668e853714ea63678d69
runningapple <runningapple8@gmail.com>
2016-02-24 10:56:29 +0800

    创建客户端 2016-2-24 10:56

---
+++ js/login.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function(){
+	$("#loginbtn").tap(function(){
+		$.ajax({
+			type:"GET",
+			
+			/*这里需要进行跨域请求，论文里可以加以展开说明*/
+			url:"http://localhost:8080/nitshare/serve/test",
+			//async:false,/*同步*/
+			jsonpCallback:'callback',
+			data:{
+				"id":"abc"
+			},
+			dataType:'jsonp',
+			success : function(data){
+				alert("ok");
+			},
+			error : function(XMLHttpRequest, textStatus, errorThrown, data){
+//				alert("本宝宝不开心了");
+				alert(errorThrown);
+			}
+		});
+	});
+});
