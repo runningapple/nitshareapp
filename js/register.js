bc9b75b73f26250912be506a2eff123e4496e2a1
runningapple <runningapple8@gmail.com>
2016-02-25 15:34:58 +0800

    添加商品展示页面，实现注册功能 2016-2-25 15:44

---
+++ js/register.js
@@ -0,0 +1,32 @@
+
+$(document).ready(function(){
+	$("#rgbtn").tap(function(){
+		var nickname = $("#nickname").val();
+		var account = $("#account").val();
+		var pwd_one = $("#pwd_one").val();
+		var pwd_two = $("#pwd_two").val();
+		if (pwd_one != pwd_two) {
+			//输入的两次密码不一致
+			alert("两次输入的密码不一致");
+			return;
+		} 
+		$.ajax({
+			type:"get",
+			url:"http://localhost:8080/nitshare/serve/user.register",
+			async:false,
+			data:{
+				"nickname":nickname,
+				"account":account,
+				"password":pwd_one
+			},
+			jsonpCallback:'callback',
+			dataType:'jsonp',
+			success:function(data){
+				alert(data);
+			},
+			error:function(XMLHttpRequest, textStatus, errorThrown, data){
+				alert(errorThrown);
+			}
+		});
+	});
+});
