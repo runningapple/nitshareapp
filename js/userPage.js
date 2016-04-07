06b39f1966a9a8cfda0e4cf3c0c81e2a5c349f3d
runningapple <runningapple8@gmail.com>
2016-04-07 10:28:58 +0800

    优化个人中心页面

--- js/userPage.js
+++ js/userPage.js
@@ -1,13 +1,17 @@
+/*防止页面滚动*/
+document.body.addEventListener('touchmove', function (event) {
+    event.preventDefault();
+}, false);
 
 $(document).ready(function(){
 	$(window).scroll(function(){
-		$(window).scrollTo(0)
+		$(window).scrollTo(0);
 	});
 	var contentWidth = $("#header",window.parent.document).width();
 	var contentHeight = $("#web-view",window.parent.document).height()-$("#header",window.parent.document).height()-$("#footer",window.parent.document).height();
 //	alert(contentHeight+" "+contentWidth+" "+$("#header").height()+" "+$("#content").height());//60 90
 	$(".contentdiv").css({"height":contentHeight+"px","width":contentWidth+"px"});
 	$("#header").css("height",contentHeight*0.4+"px");
-	$("#content").css({"top":contentHeight*0.4+"px","height":contentHeight*0.62+"px"});
-	$("#contentTable").css("height",contentHeight*0.62+"px");
+	$("#content").css({"top":contentHeight*0.4+"px","height":contentHeight*0.63+"px"});
+	$("#contentTable").css("height",contentHeight*0.63+"px");
 });
